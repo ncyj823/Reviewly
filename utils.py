280cdd9802b4a223199967be3577699aa98a0171
Nancy <nancyjha24@gmail.com>
2026-07-06 12:32:22 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+def run_command(user_input):
+    # Execute user command directly
+    result = subprocess.run(user_input, shell=True, capture_output=True)
+    return result.stdout
+
+def get_user_data(user_id):
+    query = "SELECT * FROM users WHERE id = " + user_id
+    return query
+
+def divide(a, b):
+    return a / b  
